77dbdb5b5362b53f2fc6efc4b5bf5fce93f05dd6
AStefankiv <stefankif35@gmail.com>
2024-07-15 17:25:48 -0600

    Remove links from Footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,18 +1,113 @@
+// import React from "react";
+// import LogoGV from "../../public/assets/Logo.svg";
+// import Facebook from "../../public/assets/Icon/facebook.svg";
+// import Twitter from "../../public/assets/Icon/twitter.svg";
+// import Instagram from "../../public/assets/Icon/instagram.svg";
+// const Footer = () => {
+//   return (
+//     <div className="bg-white-300 pt-16 pb-8">
+//       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+//         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+//           <LogoGV className="h-8 w-auto mb-6" />
+//           <p className="mb-4">
+//             <strong className="font-medium">Work learn travel</strong> helps Canadians aged 18 to 35 to work and travel around the world.
+//           </p>
+//           <div className="flex w-full mt-2 mb-8 -mx-2">
+//             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+//               <Facebook className="h-6 w-6" />
+//             </div>
+//             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+//               <Twitter className="h-6 w-6" />
+//             </div>
+//             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+//               <Instagram className="h-6 w-6" />
+//             </div>
+//           </div>
+//           <p className="text-gray-400">©{new Date().getFullYear()} - Work - learn - travel</p>
+//         </div>
+//         <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
+//           <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
+//           <ul className="text-black-500 ">
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Download{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Pricing{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Locations{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Server{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Countries{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Blog{" "}
+//             </li>
+//           </ul>
+//         </div>
+//         <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
+//           <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
+//           <ul className="text-black-500">
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Global Village{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               FAQ{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Tutorials{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               About Us{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Privacy Policy{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Terms of Service{" "}
+//             </li>
+//           </ul>
+//         </div>
+//         <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
+//           <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
+//           <ul className="text-black-500">
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Affiliate{" "}
+//             </li>
+//             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+//               Become Partner{" "}
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+
+
+
+
+
 import React from "react";
 import LogoGV from "../../public/assets/Logo.svg";
 import Facebook from "../../public/assets/Icon/facebook.svg";
 import Twitter from "../../public/assets/Icon/twitter.svg";
 import Instagram from "../../public/assets/Icon/instagram.svg";
+
 const Footer = () => {
   return (
-    // <div className="bg-white-300 pt-44 pb-24">
-    <div className="bg-white-300 pt-16 pb-8">
+    <div className="bg-white-300 pt-10 pb-8">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <LogoGV className="h-8 w-auto mb-6" />
           <p className="mb-4">
-            <strong className="font-medium">Work-learn-travel</strong> is a private
-            travel compay that helps Canadians of 18 - 35 years old to work and travel around the world.
+            <strong className="font-medium">Work learn travel</strong> helps Canadians aged 18 to 35 to work and travel around the world.
           </p>
           <div className="flex w-full mt-2 mb-8 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
@@ -27,60 +122,18 @@ const Footer = () => {
           </div>
           <p className="text-gray-400">©{new Date().getFullYear()} - Work - learn - travel</p>
         </div>
-        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
-          <ul className="text-black-500 ">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Download{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Pricing{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Locations{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Server{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Countries{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Blog{" "}
-            </li>
-          </ul>
-        </div>
         <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
           <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Global Village{" "}
+            <li className="my-8 hover:text-orange-500 cursor-pointer transition-all">
+              Contact Us{" "}
             </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              FAQ{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Tutorials{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+            <li className="my-8 hover:text-orange-500 cursor-pointer transition-all">
               About Us{" "}
             </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Privacy Policy{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Terms of Service{" "}
-            </li>
-          </ul>
-        </div>
-        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
-          <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
-          <ul className="text-black-500">
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Affiliate{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Become Partner{" "}
+            <li className="my-8 hover:text-orange-500 cursor-pointer transition-all">
+              <a href="https://gvenglish.com/" target="_blank" rel="noopener noreferrer">
+              Global Village{" "}
+              </a>
             </li>
           </ul>
         </div>
