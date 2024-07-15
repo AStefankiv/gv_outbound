5b3289f8799ad24cc2812a01734af331e765107a
AStefankiv <stefankif35@gmail.com>
2024-07-14 22:27:08 -0600

    Add SeoHead gv_image

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -15,7 +15,7 @@ const defaultMeta = {
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'gv_image',
+  image: gv_image,
   author: 'Andrew Stefankiv'
 };
 
