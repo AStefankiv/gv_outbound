5c8a420799862ea812d0271eaa4bda5af6e45fca
AStefankiv <stefankif35@gmail.com>
2024-10-12 10:08:52 -0600

    Add meta image to SeoHead

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -40,7 +40,7 @@ const SeoHead = (props) => {
       <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content='/public/assets/global_village.JPG' />
+      <meta name='image' property='og:image' content='https://raw.githubusercontent.com/AStefankiv/gv_outbound/refs/heads/main/public/assets/global_village.jpg' />
       {/* Twitter */}
       <meta name='twitter:card' content={meta.image} />
       <meta name='twitter:site' content='@F2aldi' />
