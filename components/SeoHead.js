26c45478d77037e73cbe04f7f79b680f4323f168
AStefankiv <stefankif35@gmail.com>
2024-10-12 09:22:13 -0600

    Add meta image to SeoHead

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -40,7 +40,8 @@ const SeoHead = (props) => {
       <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
+      <meta property='og:image' content={meta.image} />
+      {/* <meta name='image' property='og:image' content={meta.image} /> */}
       {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@F2aldi' />
