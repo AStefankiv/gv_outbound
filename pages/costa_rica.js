636c8e85f6b1e15b428e513119481825b9c003fa
AStefankiv <stefankif35@gmail.com>
2024-10-13 21:07:38 -0600

    Add Costa Rica page

--- pages/costa_rica.js
+++ pages/costa_rica.js
@@ -41,7 +41,7 @@ const CostaRica = () => {
                     alt="nature_conservation"
                     className="absolute inset-0 w-full h-full object-cover rounded-lg"
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold" style={{ color: "rgba(241,163,52,255)" }}>
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold text-center" style={{ color: "rgba(241,163,52,255)" }}>
                     NATURE CONSERVATION
                   </div>
                 </Link>
@@ -58,7 +58,7 @@ const CostaRica = () => {
                     alt="INTERNSHIP"
                     className="absolute inset-0 w-full h-full object-cover rounded-lg"
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold" style={{ color: "rgba(241,163,52,255)" }}>
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold text-center" style={{ color: "rgba(241,163,52,255)" }}>
                     COMMUNITY DEVELOPMENT
                   </div>
                 </Link>
@@ -75,7 +75,7 @@ const CostaRica = () => {
                     alt="TEACHING"
                     className="absolute inset-0 w-full h-full object-cover rounded-lg"
                   />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold" style={{ color: "rgba(241,163,52,255)" }}>
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-4xl font-bold text-center" style={{ color: "rgba(241,163,52,255)" }}>
                     TEACHING
                   </div>
                 </Link>
