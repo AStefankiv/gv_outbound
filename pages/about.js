317d6ae162f7b6313a0f9d824197da299b185ac3
AStefankiv <stefankif35@gmail.com>
2024-10-17 22:24:28 -0600

    Add content to ABOUT US page

--- pages/about.js
+++ pages/about.js
@@ -16,12 +16,29 @@ const About = () => {
                 As official partners of <strong>International Experience Canada</strong>, we’re dedicated to creating life-changing 
                 opportunities for Canadians as they work, learn, and travel all over our amazing world.
               </p>
+              <p className="text-lg mb-4">
+                We’re part of the Languages Canada Consortium – a designated recognized organization of the Government of Canada’s 
+                International Experience Canada (IEC) program. IEC gives young Canadians the opportunity to broaden their perspective 
+                of the world and Canada’s place in it through international work and travel experience.
+              </p>
+              <p className="text-lg mb-4">
+                Through IEC, Canada has negotiated temporary immigration pathways with more than 35 countries and territories that 
+                allow Canadian youth (between ages 18-35) to work while they travel in a partner country or territory. The same 
+                opportunity is provided to young citizens from partner countries and territories (and sometimes other countries) 
+                to come to Canada. This initiative originated over 60 years ago as a cultural exchange program between youth. 
+                With IEC’s current mandate, the Government of Canada remains committed to fostering cultural, economic and social 
+                exchanges between youth through work and travel opportunities.
+              </p>
+              <p className="text-lg mb-4">
+                The Government of Canada works with key stakeholders like Work Learn Travel in Canada and abroad to raise awareness 
+                of the program, to promote the benefits of international experience, and to equip Canadian and foreign national youth 
+                with information and services to help them achieve international work and travel experiences through IEC.
+              </p>
               <p className="text-lg mb-6">
                 For more information, feel free to reach out to us.
               </p>
               <a 
-                // href="mailto:gvoutbound@gvenglish.com"
-                href="mailto:stefankif35@gmail.com"
+                href="mailto:gvoutbound@gvenglish.com"
                 className="inline-block bg-orange-500 text-white font-semibold py-2 px-6 rounded hover:bg-orange-600 transition duration-300"
               >
                 Contact Us
