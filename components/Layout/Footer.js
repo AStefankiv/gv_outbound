b5b83e96eed965b45f33de8aa4d944ffb12b64ef
AStefankiv <stefankif35@gmail.com>
2024-10-24 17:36:19 -0600

    Replace footer logo to png format

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-5 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-12 gap-0">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <Image
-            src="/assets/GV-Work-Learn-Travel-Logo-Full-Logo-smoke.jpg"
+            src="/assets/GV-Work-Learn-Travel-Logo-Transparent.png"
             alt="GV Logo"
             width={200}
             height={64}
