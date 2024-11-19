225b6e39ff9bb1a002f5e8adf542c504b0c018c2
AStefankiv <stefankif35@gmail.com>
2024-11-18 21:41:31 -0700

    --

--- pages/costa_rica/nature_conservation.js
+++ pages/costa_rica/nature_conservation.js
@@ -195,11 +195,6 @@ const NatureConservation = () => {
             <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/3 xl:w-2/3 mx-auto">
               <ScrollAnimationWrapper className="w-full justify-end">
                 <motion.div className="h-full w-full p-4">
-                  <div className="my-12 text-center">
-                    <p className="text-lg text-black-500 mb-8">
-                      Fill out the form below, and we'll send you all the details.
-                    </p>
-                  </div>
 
                   {/* HubSpot form */}
                   <div id="hubspotForm"></div>
