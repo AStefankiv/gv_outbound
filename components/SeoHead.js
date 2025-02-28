8c19dff0e3adf42c5d868159b5a81f394e5a5619
AStefankiv <stefankif35@gmail.com>
2025-02-27 22:06:35 -0700

    Costa Rica useEffect

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -104,6 +104,26 @@ const SeoHead = (props) => {
         `}
       </Script>
 
+      {/* Google Ads Conversion Tracking */}
+      <Script id="google-ads-conversion" strategy="afterInteractive">
+        {`
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof url !== "undefined") {
+                window.location = url;
+              }
+            };
+            window.gtag("event", "conversion", {
+              send_to: "AW-16884794738/uY75CLi2uKAaEPKKpvM-",
+              value: 1.0,
+              currency: "CAD",
+              event_callback: callback,
+            });
+            return false;
+          }
+        `}
+      </Script>
+
       {/* HubSpot Script using next/script */}
       <Script
         id="hs-script-loader"
